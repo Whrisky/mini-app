12cf674c7d0188e9a15aad4c6c344757ed1a88f4
Whrisky <yym1995@buaa.edu.cn>
2018-07-17 11:18:57 +0800

    init

---
+++ src/main/java/com/yym/boot/miniapp/MiniAppApplication.java
@@ -0,0 +1,12 @@
+package com.yym.boot.miniapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAppApplication.class, args);
+	}
+}
