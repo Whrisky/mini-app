5062d186d1f54fb5c6ba90021d24f6edef3056d9
Whrisky <yym1995@buaa.edu.cn>
2018-07-17 11:39:58 +0800

    annoations

---
+++ src/main/java/com/yym/boot/miniapp/annoations/NotRecomment.java
@@ -0,0 +1,13 @@
+package com.yym.boot.miniapp.annoations;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecomment {
+    String value() default "";
+}
